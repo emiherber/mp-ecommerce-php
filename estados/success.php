1dc6a56dab865f0bcd814e35e39b6f21d9f04194
Emiliano <emiherbes@hotmail.com>
2020-06-11 17:38:50 -0300

    arreglo4

--- estados/success.php
+++ estados/success.php
@@ -5,6 +5,11 @@ print_r($_GET);
 echo '</pre>';
 
 $urlNoti = __PathUrl__ . 'notificacion.php?id='.$_GET['collection_id'];
+
+
+echo '<br> <pre>';
+print_r($_POST);
+echo '</pre>';
 ?>
 
 <a href="<?=$urlNoti?>">Ver notificacion</a>
