03d643f42f838e78f9d0f5f0de880e99bae34e23
Emiliano <emiherbes@hotmail.com>
2020-06-11 16:12:01 -0300

    notificicacion

--- notificacion.php
+++ notificacion.php
@@ -1,16 +1,33 @@
 <?php
+require_once __DIR__ . '/includes/MercadoPago/MercadoPago.php';
 require_once __DIR__ . '/includes/ErrorLog.php'; 
-http_response_code(200);
-echo '<br>inicio post <br>';
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-echo '<br>fin post <br>';
-echo '<br>inicio get <br>';
-echo '<pre>';
-print_r($_GET);
-echo '</pre>';
-echo '<br>fin get <br>';
+require_once 'constantes.php';
 
-ErrorLog::log('notificacionPOST', '', '', $_POST);
-ErrorLog::log('notificacionGET', '', '', $_GET);
+MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
+//        MercadoPago\SDK::setClientId('469485398');
+        MercadoPago\SDK::setPublicKey(__publickey__);
+        MercadoPago\SDK::setAccessToken(__token__);
+
+//$url = 'https://api.mercadopago.com/v1/payments/7059869322';
+//
+//$url .= 'access_token='.__token__;
+//
+//$ch = curl_init($url);
+//
+////set the content type to application/json
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+//
+////return response instead of outputting
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+////execute the POST request
+//$result = curl_exec($ch);
+////close cURL resource
+//curl_close($ch);
+//
+//ErrorLog::log('notificacion',$result);
+$result = MercadoPago\Payment::find_by_id($_POST["id"]);
+http_response_code(200);
+echo '<pre>';
+print_r($result);
+echo '</pre>';
